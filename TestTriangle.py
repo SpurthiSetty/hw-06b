132a6d07e9d03bcad3ec6ef77b98901cd06ee0bc
SpurthiSetty <spurthi.setty@gmail.com>
2025-03-14 20:50:37 -0400

    enhanced test cases

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,32 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
-
-@author: jrr
-@author: rk
-"""
-
 import unittest
-
 from Triangle import classifyTriangle
 
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+# filepath: c:\Spurthi\Stevens\Classes\Junior Year Spring\SSW - 567\hw-06b\test_TestTriangle.py
 
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3, 4, 5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5, 3, 4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral', '1, 1, 1 should be equilateral')
+
+    def testIsoscelesTriangle(self):
+        self.assertEqual(classifyTriangle(2, 2, 3), 'Isosceles', '2, 2, 3 should be isosceles')
+
+    def testScaleneTriangle(self):
+        self.assertEqual(classifyTriangle(3, 4, 6), 'Scalene', '3, 4, 6 should be scalene')
+
+    def testInvalidTriangleA(self):
+        self.assertEqual(classifyTriangle(0, 0, 0), 'InvalidInput', '0, 0, 0 should be invalid')
+
+    def testInvalidTriangleB(self):
+        self.assertEqual(classifyTriangle(-1, 2, 3), 'InvalidInput', '-1, 2, 3 should be invalid')
+
+    def testNotATriangle(self):
+        self.assertEqual(classifyTriangle(1, 10, 12), 'NotATriangle', '1, 10, 12 should not form a triangle')
 
 if __name__ == '__main__':
-    print('Running unit tests')
     unittest.main()
-
